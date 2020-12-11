3844b13f35bfd525d6272f86f4562ee112240554
女神 <53878781+RomanGoddess@users.noreply.github.com>
2020-12-11 14:41:46 +0100

    Update auto_forward.py

--- forwarder/modules/auto_forward.py
+++ forwarder/modules/auto_forward.py
@@ -14,7 +14,7 @@ def forward(bot: Bot, update: Update):
     for chat in TO_CHATS:
         to_chat_name = bot.get_chat(chat).title or bot.get_chat(chat).first_name
         try:
-            bot.send_message(chat_id=chat, from_chat_id=from_chat_id, message_id=message.message_id)
+            bot.forward_message(chat_id=chat, from_chat_id=from_chat_id, message_id=message.message_id)
         
         except:
             LOGGER.exception("Error while sending message from chat \"{}\" to chat \"{}\".".\
